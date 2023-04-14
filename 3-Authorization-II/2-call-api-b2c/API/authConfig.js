f3a17cd6d6e3f84a8f7930974fe88f2f2223e63d
Sagar Nagadiya <sanagadi@microsoft.com>
2023-04-14 18:48:31 +0530

    My changes

--- 3-Authorization-II/2-call-api-b2c/API/authConfig.js
+++ 3-Authorization-II/2-call-api-b2c/API/authConfig.js
@@ -1,34 +1,33 @@
 const passportConfig = {
-    credentials: {
-        tenantName: 'fabrikamb2c.onmicrosoft.com',
-        clientID: 'e29ac359-6a90-4f9e-b31c-8f64e1ac20cb',
+  credentials: {
+    tenantName: "uppercircuit.onmicrosoft.com",
+    clientID: "a29877a2-99d6-4a99-9096-e0c8c132a5d5",
+  },
+  policies: {
+    policyName: "B2C_1_signupsignin1",
+  },
+  metadata: {
+    b2cDomain: "uppercircuit.b2clogin.com",
+    authority: "login.microsoftonline.com",
+    discovery: ".well-known/openid-configuration",
+    version: "v2.0",
+  },
+  settings: {
+    isB2C: true,
+    validateIssuer: false,
+    passReqToCallback: true,
+    loggingLevel: "info",
+    loggingNoPII: false,
+  },
+  protectedRoutes: {
+    todolist: {
+      endpoint: "/api/todolist",
+      delegatedPermissions: {
+        read: ["myblog.read"],
+        write: ["myblog.write"],
+      },
     },
-    policies: {
-        policyName: 'B2C_1_susi_v2',
-    },
-    metadata: {
-        b2cDomain: 'fabrikamb2c.b2clogin.com',
-        authority: 'login.microsoftonline.com',
-        discovery: '.well-known/openid-configuration',
-        version: 'v2.0',
-    },
-    settings: {
-        isB2C: true,
-        validateIssuer: false,
-        passReqToCallback: true,
-        loggingLevel: 'info',
-        loggingNoPII: false,
-    },
-    protectedRoutes: {
-        todolist: {
-            endpoint: '/api/todolist',
-            delegatedPermissions: {
-                read: ['ToDoList.Read', 'ToDoList.ReadWrite'],
-                write: ['ToDoList.ReadWrite'],
-            },
-        },
-    },
+  },
 };
 
 module.exports = passportConfig;
-
